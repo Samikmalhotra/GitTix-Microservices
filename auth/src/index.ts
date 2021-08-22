4960e6e72ab074799a9b2d95fa7aa89722dca985
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-22 20:35:56 +0530

    added current-user.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import {currentUserRouter} from './routes/current-user';
 
 const app = express();
 app.use(express.json());
 
-app.get('/api/users/currentUser', (req,res)=>{
-  res.json('Hello')
-})
+app.use(currentUserRouter);
 
 app.listen(3333, () => {
   console.log('Listening on port 3333!!!')
