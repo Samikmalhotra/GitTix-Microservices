3c6a648d7deec31304be1cc6313ef680c7b5239c
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-22 19:06:06 +0530

    added ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 
 const app = express();
-
 app.use(express.json());
+
+app.get('/api/users/currentUser', (req,res)=>{
+  res.json('Hello')
+})
 
 app.listen(3333, () => {
   console.log('Listening on port 3333!!!')
