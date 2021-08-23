c5039219da5d041d396fc3754c2b4dfc2f0a59d9
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-23 09:34:36 +0530

    added routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import {currentUserRouter} from './routes/current-user';
+import {signInRouter} from './routes/signin';
+import {signOutRouter} from './routes/signout';
+import {signUpRouter} from './routes/signup';
 
 const app = express();
 app.use(express.json());
 
 app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
 
 app.listen(3333, () => {
   console.log('Listening on port 3333!!!')
