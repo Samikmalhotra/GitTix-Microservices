7e629ba48767a039aafd9c0517d8636110b1f241
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-30 19:26:33 +0530

    initialised cookieSession

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors'
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 import {currentUserRouter} from './routes/current-user';
 import {signInRouter} from './routes/signin';
 import {signOutRouter} from './routes/signout';
@@ -10,7 +11,15 @@ import { NotFoundError } from './errors/not-found-error';
 import { DatabaseConnectionError } from './errors/database-connection-error';
 
 const app = express();
+
+app.set('trust proxy', true);
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+)
 
 app.use(currentUserRouter);
 app.use(signInRouter);
