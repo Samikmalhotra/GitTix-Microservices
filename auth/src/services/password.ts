ec6eb62bb41041be6a0de26ea7e3e9dac6c99982
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-30 17:50:58 +0530

    hashed password

---
+++ auth/src/services/password.ts
@@ -0,0 +1,11 @@
+import bcrypt from 'bcrypt';
+
+export class Password{
+  static toHash(password: string): string {
+    return bcrypt.hashSync(password, 8);
+  }
+
+  static compare(password: string, hash: string): boolean {
+    return bcrypt.compareSync(password, hash);
+  }
+}
