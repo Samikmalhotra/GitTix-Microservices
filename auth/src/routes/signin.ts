4cf7ab6a6e6acd70d1a37a9656377fe45e32381d
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-31 13:04:57 +0530

    added errors in signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,10 +1,24 @@
-import express from 'express'
-import {Request,Response} from 'express'
+import express, {Request,Response} from 'express'
+import { body, validationResult } from 'express-validator'
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router()
 
-router.post('/api/users/signin', (req: Request, res: Response) => {
-  res.json('Hello');
-})
+router.post('/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('You must supply a password')
+  ], 
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    const {email, password} = req.body
+    console.log(email, password)
+  }
+)
 
 export { router as signInRouter};
