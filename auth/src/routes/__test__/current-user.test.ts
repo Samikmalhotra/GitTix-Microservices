8504a0f6434a441356ea39b672ead1d3909f3f30
Samikmalhotra <samikmalhotra66@gmail.com>
2021-09-01 01:46:02 +0530

    completed auth route tests

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest';
 import { app } from '../../app';
 
+
+
 it('responds with details about the current user', async () => {
   const authResponse = await request(app)
     .post('/api/users/signup')
@@ -18,4 +20,13 @@ it('responds with details about the current user', async () => {
     .expect(200);
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
+});
+
+it('responds with null if not authenticated', async() => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
 });
