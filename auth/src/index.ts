09dc86814742ee7ef932d977fe6d5630a31fc67b
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-31 12:25:33 +0530

    updated cookie

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -33,6 +33,9 @@ app.all('*', async() => {
 app.use(ErrorHandler);
 
 const start = async () => {
+  if(!process.env.JWT_KEY){
+    throw new Error('JWT_KEY must be defined')
+  }
   try {
     await mongoose.connect('mongodb://auth-mongo-svc:27017/auth');
     console.log('Connected to MongoDB');
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -34,7 +34,7 @@ router.post('/api/users/signup',[
   const userJwt = jwt.sign({
     id: user.id,
     email: user.email
-  },'asdf');
+  },process.env.JWT_KEY!);
 
   // Store it on session object
   req.session = {
