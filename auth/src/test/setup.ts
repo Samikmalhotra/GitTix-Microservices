a4b06575bfd350a729d5e42e127f5d8c0938f2a5
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-31 20:23:06 +0530

    initial test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -5,6 +5,8 @@ import {app} from '../app';
 let mongo: any;
 
 beforeAll(async () => {
+  process.env.JWT_KEY = 'abcd';
+
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
