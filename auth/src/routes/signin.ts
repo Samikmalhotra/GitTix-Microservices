bb874e31ee71a101e1e3676ff7e63455a75355f3
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-31 14:05:50 +0530

    validate request middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,7 @@
 import express, {Request,Response} from 'express'
 import { body, validationResult } from 'express-validator'
 import { RequestValidationError } from '../errors/request-validation-error';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router()
 
@@ -9,13 +10,8 @@ router.post('/api/users/signin',
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().notEmpty().withMessage('You must supply a password')
   ], 
-  (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
+  validateRequest,
+  async(req: Request, res: Response) => {
     const {email, password} = req.body
     console.log(email, password)
   }
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -6,6 +6,7 @@ import {User} from '../models/user'
 import {DatabaseConnectionError} from '../errors/database-connection-error'
 import {RequestValidationError} from '../errors/request-validation-error'
 import { BadRequestError } from '../errors/bad-request-error'
+import { validateRequest } from '../middlewares/validate-request'
 
 const router = express.Router()
 
@@ -13,12 +14,9 @@ const router = express.Router()
 router.post('/api/users/signup',[
   body('email').isEmail().withMessage('Email must be valid'),
   body('password').trim().isLength({min:4, max:20}).withMessage('Password must be between 4 and 20 characters')
-], async(req: Request, res: Response) => {
-  const errors = validationResult(req);
-
-  if(!errors.isEmpty()){
-    throw new RequestValidationError(errors.array())
-  }
+],
+  validateRequest,
+  async(req: Request, res: Response) => {
 
   const {email,password} = req.body;
   const existingUser = await User.findOne({email})
