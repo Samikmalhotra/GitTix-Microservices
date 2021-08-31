8206d01a7f2b291c2aefff0a6da8626cdd1c86ce
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-31 14:32:41 +0530

    get current user from cookie

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,20 @@
 import express from 'express'
 import {Request,Response} from 'express'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
 router.get('/api/users/currentUser', (req: Request, res: Response) => {
-  res.json('Hello');
+  if(!req.session?.jwt){
+    return res.json({currentUser: null})
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.json({currentUser: payload})
+  } catch (error) {
+    res.json({currentUser: null});
+  }
 })
 
 export { router as currentUserRouter};
