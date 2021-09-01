71dceb9545b064c1182cade5b9026919d7b5c5b4
Samikmalhotra <samikmalhotra66@gmail.com>
2021-09-01 10:29:24 +0530

    updated next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 300,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
