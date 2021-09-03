79bf8c3da7d7d363955334b80d19eb23aad5d43c
Samikmalhotra <samikmalhotra66@gmail.com>
2021-09-03 23:06:51 +0530

    implemented auth

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 import { AxiosResponse } from 'axios'
 import buildClient from '../api/build-client'
 
-const LandingPage: NextPage = (props) => {
-  console.log(props)
-  return (
-    <h1>Landing Page</h1>
-  )
+interface Props {
+  currentUser?: object | null
+}
+
+const LandingPage: NextPage<Props> = ({currentUser}) => {
+  return currentUser ? <h1>You are Signed in</h1> : <h1>You are not signed in</h1>
 }
 
 LandingPage.getInitialProps = async (context) => {
