5b8bfd8463ca6c30521a2292f39d0e145a62e05a
Samikmalhotra <samikmalhotra66@gmail.com>
2021-09-04 01:17:42 +0530

    made new interface

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,35 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppContext, AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from '../api/build-client'
+import { Fragment } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface User{
+  email: string,
+  password: string
 }
-export default MyApp
+interface AppPropsExtension extends AppProps {
+  user?: User
+}
+
+const App = ({ Component, pageProps, user }: AppPropsExtension ) => {
+  return (
+    <Fragment>
+    <h1>Header! {user?.email}</h1>
+    <Component {...pageProps} />
+    </Fragment>
+  )
+}
+
+App.getInitialProps = async (context: AppContext) => {
+  const { ctx } = context
+  const client = buildClient(ctx)
+  const { data } = await client.get('/api/users/currentuser')
+  let pageProps = {}
+  if (context.Component.getInitialProps) {
+    pageProps = await context.Component.getInitialProps(ctx)
+  }
+  return { pageProps, user: data.currentUser }
+}
+
+export default App
