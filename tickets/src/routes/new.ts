d9039da37afa2261629af05a0517ee5e648704b5
Samikmalhotra <samikmalhotra66@gmail.com>
2021-09-07 11:28:18 +0530

    tests passed

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,9 +1,19 @@
 import express, {Request, Response} from 'express';
-import { requireAuth } from '@samikmalhotra/microservices-helper';
+import { requireAuth, validateRequest } from '@samikmalhotra/microservices-helper';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req:Request, res:Response)=>{
+router.post('/api/tickets', requireAuth, [
+  body('title')
+    .not()
+    .isEmpty()
+    .withMessage('Title is required'),
+  body('price')
+    .isFloat({gt: 0})
+    .withMessage('Price must be greater than 0')
+
+], validateRequest, (req:Request, res:Response)=>{
   res.sendStatus(200);
 })
 
