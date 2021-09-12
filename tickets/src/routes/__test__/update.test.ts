cf30539eb20feba300a1e397ceb6f0b015154e95
Samikmalhotra <samikmalhotra66@gmail.com>
2021-09-12 21:21:27 +0530

    added update tests

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,8 +1,19 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
+import { signinTest } from '../../test/setup';
 
 it('returns a 404 if the provided id does not exist', async()=>{
+  const id = new mongoose.Types.ObjectId().toHexString();
 
+  await request(app)
+      .put(`/api/tickets/${id}`)
+      .set('Cookie', signinTest())
+      .send({
+          title: 'asdf',
+          price: 20
+      })
+      .expect(404);
 })
 
 it('returns a 401 if the user is not authenticated', async()=>{
@@ -11,4 +22,12 @@ it('returns a 401 if the user is not authenticated', async()=>{
 
 it('returns a 401 if the user does not own the ticket', async()=>{
   
+})
+
+it('returns a 400 if the user provides an invalid title or price', async()=>{
+  
+})
+
+it('updates the ticket provided valid inputs', async()=>{
+
 })
