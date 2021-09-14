1d03d31f2ed4ea7c25b22161d2fea845cddcf3e6
Samikmalhotra <samikmalhotra66@gmail.com>
2021-09-15 03:41:59 +0530

    publisher classes

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import {TicketCreatedPublisher} from './events/ticket-created-publisher';
 
 console.clear();
 
@@ -9,20 +10,29 @@ const stan = nats.connect('ticketing','abc', {
 stan.on('connect', () => {
   console.log('Publisher connected to NATS');
 
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({
+    id: '123',
+    title: 'concert',
+    price: 20
+  })
+
   // stan.on('close', () => {
   //   console.log('NATS connection closed');
   //   process.exit();
   // });
 
   // publish a message
-  const data = JSON.stringify({
-    id: '123',
-    title: 'concert',
-    price: 20
-  });
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 20
+  // });
 
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-    // stan.close();
-  });
+  // stan.publish('ticket:created', data, () => {
+  //   console.log('Event published');
+  //   // stan.close();
+  // });
+
+
 });
