98a87465e90a44c08a8c75c78d49eddd4cc4ddbc
Samikmalhotra <samikmalhotra66@gmail.com>
2021-09-15 02:35:15 +0530

    listener class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, {Message} from 'node-nats-streaming';
+import nats, {Message, Stan} from 'node-nats-streaming';
 import {randomBytes} from 'crypto';
 
 console.clear();
@@ -15,7 +15,12 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const options = stan
+      .subscriptionOptions()
+      .setManualAckMode(true)
+      .setDeliverAllAvailable()
+      .setDurableName('accounting-service');
+
   const subscription = stan.subscribe('ticket:created',
   'orders-service-queue-group',
   options
@@ -31,12 +36,47 @@ stan.on('connect', () => {
     msg.ack();
   })
 
-  
-
-  process.on('SIGINT', () => stan.close());
-  process.on('SIGTERM', () => stan.close());
-
   // stan.subscribe('ticket:created', (msg: any) => {
   //   console.log(`Processing a new ticket: ${msg.data}`);
   // });
 })
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
+
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
+  private client: Stan;
+  protected ackWait = 5 * 1000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client.subscriptionOptions()
+        .setDeliverAllAvailable()
+        .setManualAckMode(true)
+        .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(this.subject, this.queueGroupName,
+        this.subscriptionOptions());
+
+    subscription.on('message', (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string' ? JSON.parse(data) : JSON.parse(data.toString('utf8'));
+  }
+}
