cbde5caa34237d0efd48f6a223bf22c8fc9ecbd2
Samikmalhotra <samikmalhotra66@gmail.com>
2021-09-16 23:54:37 +0530

    added ticket creation event

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,6 +12,13 @@ const start = async () => {
   }
   try {
     await natsWrapper.connect('ticketing', 'dasdas', 'http://nats-svc:4222')
+    natsWrapper.client.on('close', ()=> {
+      console.log('NATS connection closed');
+      process.exit();
+    });
+    process.on('SIGINT', ()=> natsWrapper.client.close());
+    process.on('SIGTERM', ()=> natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (error) {
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { requireAuth, validateRequest } from '@samikmalhotra/microservices-helpe
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher'
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -24,7 +25,7 @@ router.post('/api/tickets', requireAuth, [
     userId: req.currentUser!.id
   })
   await ticket.save();
-  new TicketCreatedPublisher(client).publish({
+  new TicketCreatedPublisher(natsWrapper.client).publish({
     id: ticket.id,
     title: ticket.title,
     price: ticket.price,
