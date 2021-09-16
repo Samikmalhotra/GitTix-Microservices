f01523f75c4835fd464008865c56291b179451aa
Samikmalhotra <samikmalhotra66@gmail.com>
2021-09-16 21:34:25 +0530

    added ticket creted publisher

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 import { requireAuth, validateRequest } from '@samikmalhotra/microservices-helper';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher'
 
 const router = express.Router();
 
@@ -23,6 +24,12 @@ router.post('/api/tickets', requireAuth, [
     userId: req.currentUser!.id
   })
   await ticket.save();
+  new TicketCreatedPublisher(client).publish({
+    id: ticket.id,
+    title: ticket.title,
+    price: ticket.price,
+    userId: ticket.userId
+  })
 
   res.status(201).send(ticket);
 })
