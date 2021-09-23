8a49e55d9132cb141e552c5de7eddee51f515fb7
Samikmalhotra <samikmalhotra66@gmail.com>
2021-09-24 03:01:32 +0530

    added routes to app

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import 'express-async-errors'
 import cookieSession from 'cookie-session';
 import { ErrorHandler, NotFoundError, currentUser } from '@samikmalhotra/microservices-helper';
-import { createTicketRouter } from './routes/new';
-import {showTicketRouter} from './routes/show';
-import { indexTicketRouter } from './routes';
-import { updateTicketRouter } from './routes/update';
+import { newOrderRouter } from './routes/new';
+import {showOrderRouter} from './routes/show';
+import { indexOrderRouter } from './routes';
+import { deleteOrderRouter } from './routes/delete';
 
 const app = express();
 
@@ -18,10 +18,10 @@ app.use(
   })
 )
 app.use(currentUser);
-app.use(createTicketRouter)
-app.use(showTicketRouter)
-app.use(indexTicketRouter)
-app.use(updateTicketRouter)
+app.use(newOrderRouter)
+app.use(showOrderRouter)
+app.use(indexOrderRouter)
+app.use(deleteOrderRouter)
 
 app.all('*', async() => {
   throw new NotFoundError();
