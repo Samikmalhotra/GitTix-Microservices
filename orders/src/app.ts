1b035e89f30634d6e2934df66cfd65d1323f05b8
Samikmalhotra <samikmalhotra66@gmail.com>
2021-09-24 02:41:29 +0530

    setting up orders pt 2

---
+++ orders/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import 'express-async-errors'
+import cookieSession from 'cookie-session';
+import { ErrorHandler, NotFoundError, currentUser } from '@samikmalhotra/microservices-helper';
+import { createTicketRouter } from './routes/new';
+import {showTicketRouter} from './routes/show';
+import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+)
+app.use(currentUser);
+app.use(createTicketRouter)
+app.use(showTicketRouter)
+app.use(indexTicketRouter)
+app.use(updateTicketRouter)
+
+app.all('*', async() => {
+  throw new NotFoundError();
+})
+
+app.use(ErrorHandler);
+
+
+export {app};
---
+++ orders/src/index.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+import { DatabaseConnectionError } from '@samikmalhotra/microservices-helper';
+import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+
+const start = async () => {
+  if(!process.env.JWT_KEY){
+    throw new Error('JWT_KEY must be defined')
+  }
+  if(!process.env.MONGO_URI){
+    throw new Error('MONGO_URI must be defined')
+  }
+  if(!process.env.NATS_CLIENT_ID){
+    throw new Error('NATS_CLIENT_ID must be defined')
+  }
+  if(!process.env.NATS_URL){
+    throw new Error('NATS_URL must be defined')
+  }
+  if(!process.env.NATS_CLUSTER_ID){
+    throw new Error('NATS_CLUSTER_ID must be defined')
+  }
+  try {
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID , process.env.NATS_CLIENT_ID)
+    natsWrapper.client.on('close', ()=> {
+      console.log('NATS connection closed');
+      process.exit();
+    });
+    process.on('SIGINT', ()=> natsWrapper.client.close());
+    process.on('SIGTERM', ()=> natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error(error);
+    throw new DatabaseConnectionError();
+  }
+  app.listen(3333, () => {
+    console.log('Listening on port 3333!!!')
+  })
+};
+
+start();
