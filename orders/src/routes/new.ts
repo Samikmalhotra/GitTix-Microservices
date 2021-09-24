3eae1d7f291dd4eb6cebc5b2835b377429dbaa8d
Samikmalhotra <samikmalhotra66@gmail.com>
2021-09-24 07:36:44 +0530

    updated create order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,12 @@
 import express, {Request, Response} from 'express'
+import { requireAuth, validateRequest } from '@samikmalhotra/microservices-helper';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async(req:Request, res:Response)=>{
+router.post('/api/orders',requireAuth, [
+  body('ticketId').not().isEmpty().withMessage('TicketId must be provided')
+], async(req:Request, res:Response)=>{
   res.json({});
 });
 
