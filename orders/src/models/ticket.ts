0d51314aae970aef9b33ddb85478bf8b7021cf8a
Samikmalhotra <samikmalhotra66@gmail.com>
2021-10-05 01:33:00 +0530

    fixed tests

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,58 +1,56 @@
 import mongoose from 'mongoose';
-import {Order, OrderStatus} from './order'
+import { Order, OrderStatus } from './order';
 
 interface TicketAttrs {
   title: string;
   price: number;
 }
 
-interface TicketDoc extends mongoose.Document{
+export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
-  isReserved(): Promise<boolean>
+  isReserved(): Promise<boolean>;
 }
 
-interface TicketModel extends mongoose.Model<TicketDoc>{
+interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
 }
 
-const ticketSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
   },
-  price:{
-    type: Number,
-    required: true,
-    min: 0
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
   }
-}, {
-  toJSON: {
-    transform(doc, ret){
-      ret.id =ret._id;
-      delete ret._id;
-    }
-  }
-});
+);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-  return new Ticket(attrs)
-}
-ticketSchema.methods.isReserved = async function(){
+  return new Ticket(attrs);
+};
+ticketSchema.methods.isReserved = async function () {
+  // this === the ticket document that we just called 'isReserved' on
   const existingOrder = await Order.findOne({
-    ticket: ticket,
-    status:{
-      $in: [
-        OrderStatus.Created,
-        OrderStatus.AwaitingPayment,
-        OrderStatus.Complete
-      ]
-    }
-  })
+    ticket: this,
+    status: OrderStatus.Created || OrderStatus.AwaitingPayment || OrderStatus.Complete
+  });
 
   return !!existingOrder;
-}
+};
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
 
-export {Ticket, TicketDoc}
+export { Ticket };
