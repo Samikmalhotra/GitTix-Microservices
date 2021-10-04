c285f3f9cc7bc278935311d06c1765592e467d4a
Samikmalhotra <samikmalhotra66@gmail.com>
2021-10-04 06:48:30 +0530

    order creation logic

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,12 +1,30 @@
 import express, {Request, Response} from 'express'
-import { requireAuth, validateRequest } from '@samikmalhotra/microservices-helper';
+import { NotFoundError, requireAuth, validateRequest } from '@samikmalhotra/microservices-helper';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
 router.post('/api/orders',requireAuth, [
   body('ticketId').not().isEmpty().withMessage('TicketId must be provided')
 ], async(req:Request, res:Response)=>{
+  const {ticketId} = req.body;
+
+  // Find the ticket the user is trying to order in the database
+  const ticket = await Ticket.findById(ticketId);
+  if(!ticket){
+    throw new NotFoundError();
+  }
+
+  // Make sure that this ticket is not already reserved
+
+  // Calculate an expiration date for this order
+
+  // Build the order and save it to the database
+
+  // Publish an event saying that an order was created
+
   res.json({});
 });
 
