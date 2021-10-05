4b680299f9ba145951d139eebf332a8ec3907687
Samikmalhotra <samikmalhotra66@gmail.com>
2021-10-05 06:15:33 +0530

    updated show order route

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,8 +1,20 @@
+import { NotFoundError, NotAuthorizedError } from '@samikmalhotra/microservices-helper';
 import express, {Request, Response} from 'express'
+import { Order } from '../models/order';
 
 const router = express.Router();
 
 router.get('/api/orders/:orderId', async(req:Request, res:Response)=>{
+  const order = await Order.findById(req.params.orderId).populate('ticket');
+  if(!order){
+    throw new NotFoundError();
+  }
+  if(order.userId !== req.currentUser!.id){
+    throw new NotAuthorizedError();
+  }
+
+  }
+
   res.json({});
 });
 
