ced2d1b174032b307380b5006f399973bdf777c6
Samikmalhotra <samikmalhotra66@gmail.com>
2021-10-08 21:46:32 +0530

    added new test

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -1,6 +1,6 @@
 import { Ticket } from '../ticket';
 
-it('implements optimistic concurrency control', async (done) => {
+it('implements optimistic concurrency control', async () => {
   // Create an instance of the ticket
   const ticket = Ticket.build({
     title: 'concert',
@@ -26,9 +26,24 @@ it('implements optimistic concurrency control', async (done) => {
   try {
     await secondInstance!.save();
   } catch (err) {
-    return done();
+    return ;
   }
 
   throw new Error('Should not reach this point'); 
 
+})
+
+it('increments the version number on multiple saves', async()=>{
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: '123',
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
 })
