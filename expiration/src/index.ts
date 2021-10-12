b00cade72c397d18c806affbae4956e662c694a8
Samikmalhotra <samikmalhotra66@gmail.com>
2021-10-12 06:35:34 +0530

    updated index

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,17 +1,7 @@
-import mongoose from 'mongoose';
 import { DatabaseConnectionError } from '@samikmalhotra/microservices-helper';
-import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
-import { OrderCreatedListener } from './events/listeners/order-created-listener';
-import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
-  if(!process.env.JWT_KEY){
-    throw new Error('JWT_KEY must be defined')
-  }
-  if(!process.env.MONGO_URI){
-    throw new Error('MONGO_URI must be defined')
-  }
   if(!process.env.NATS_CLIENT_ID){
     throw new Error('NATS_CLIENT_ID must be defined')
   }
@@ -30,18 +20,12 @@ const start = async () => {
     process.on('SIGINT', ()=> natsWrapper.client.close());
     process.on('SIGTERM', ()=> natsWrapper.client.close());
 
-    new OrderCreatedListener(natsWrapper.client).listen();
-    new OrderCancelledListener(natsWrapper.client).listen();
 
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('Connected to MongoDB');
   } catch (error) {
     console.error(error);
     throw new DatabaseConnectionError();
   }
-  app.listen(3333, () => {
-    console.log('Listening on port 3333!!!')
-  })
+
 };
 
 start();
